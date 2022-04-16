c08dba59c6ec16184af1d0b8195e8d8f6e160e97
Foysal <emon.bigha@gmail.com>
2022-04-16 13:15:42 +0600

    cant implement image picking logic in new post

--- lib/view/newPostScreen.dart
+++ lib/view/newPostScreen.dart
@@ -1,4 +1,7 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
+import 'package:image_picker/image_picker.dart';
 class NewPostScreen extends StatefulWidget {
   const NewPostScreen({Key? key}) : super(key: key);
 
@@ -13,12 +16,13 @@ class _NewPostScreenState extends State<NewPostScreen> {
     'Share',
     'Request'
   ];
+  Image _image = Image(image: AssetImage('assets/addImage.png'), height: 150, width: 150,);
 
   @override
   Widget build(BuildContext context) {
     return SingleChildScrollView(
       child: Column(
-        crossAxisAlignment: CrossAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.center,
         children: [
           Padding(
             padding: EdgeInsets.all(8.0),
@@ -47,8 +51,24 @@ class _NewPostScreenState extends State<NewPostScreen> {
           //     }).toList(),
           //   ),
           // ),
+          Card(
+            child: SizedBox(
+              height: 200,
+              width: 200,
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  _image,
+                  IconButton(
+                    icon: Icon(Icons.camera_alt_outlined),
+                    onPressed: getImage
+                  )
+                ],
+              ),
+            ),
+          ),
           Padding(
-            padding: EdgeInsets.all(10.0),
+            padding: const EdgeInsets.all(10.0),
             child: TextField(
               decoration: InputDecoration(
                 hintText: 'Name of the book'
@@ -91,6 +111,13 @@ class _NewPostScreenState extends State<NewPostScreen> {
         ],
       ),
     );
+  }
+
+  Future getImage() async {
+    Image image = ImagePicker().pickImage(source: ImageSource.gallery) as Image ;
+    setState(() {
+      _image = image;
+    });
   }
 
 }
